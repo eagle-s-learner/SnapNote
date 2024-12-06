52c3a738c8844882bef2b2f056e1ca38fa8d1c47
satyam kr <satyam.9242@gmail.com>
2024-12-06 21:40:38 +0530

    user can like own post

--- server/controllers/handleUserLikeUnlike.js
+++ server/controllers/handleUserLikeUnlike.js
@@ -7,6 +7,57 @@ const jwtSecret = process.env.JWT_SECRET;
 
 async function userLike(req, res) {
     console.log("user like");
+    const { token } = req.cookies;
+    const { user_id, post_id } = req.body;
+
+    try {
+        if (token) {
+            jsonwebtoken.verify(token, jwtSecret, {}, async (err, user) => {
+                if (err) throw err;
+
+                await sequelize.query(
+                    `INSERT INTO likes (user_id, post_id) VALUES (:user_id, :id)`,
+                    {
+                        replacements: { user_id: user_id, id: post_id },
+                        type: Sequelize.QueryTypes.DELETE,
+                    }
+                );
+
+                const posts = await sequelize.query(
+                    `SELECT 
+                        posts.id AS post_id,
+                        posts.user_id AS post_user_id,
+                        posts.text_content,
+                        posts.image_url,
+                        COUNT(likes.id) AS total_likes,
+                        MAX(CASE WHEN likes.user_id = :userEmail THEN 1 ELSE 0 END) AS user_has_liked
+                    FROM 
+                        posts
+                    LEFT JOIN 
+                        likes
+                    ON 
+                        posts.id = likes.post_id
+                    WHERE 
+                        posts.user_id = :userEmail
+                    GROUP BY 
+                        posts.id, posts.user_id, posts.text_content, posts.image_url
+                    ORDER BY 
+                        posts.createdAt DESC;`,
+                    {
+                        replacements: { userEmail: user.email },
+                        type: Sequelize.QueryTypes.SELECT,
+                    }
+                );
+
+                // console.log(posts);
+
+                res.status(200).json({ posts });
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to unlike post" });
+    }
 }
 
 async function userUnlike(req, res) {
